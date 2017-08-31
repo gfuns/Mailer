160542ea80530ed46929fdaa920df02e16009323
Orie Chinedu Emmanuel <oriechinedu@gmail.com>
2017-08-31 10:00:16 +0100

    modified example.php

--- example.php
+++ example.php
@@ -6,10 +6,10 @@ require 'mailer.php';
 $mail = new Mailer;
 $subject = "Testing mailer";
 $body    = '<h1> Hello</h1>';
-$reciever = "oriebizline@gmail.com";
-$reciver_name = "orie chinedu";
-$sendFromEmail = "oriechinedu@gmail.com";
-$sendFromName  = "orient";
+$reciever = "reciver@gmail.com";
+$reciver_name = "Reciever Name";
+$sendFromEmail = "yourgmail@gmail.com";
+$sendFromName  = "yourName";
 
 if ($mail->sendMail("$subject", $body, $reciever, $reciver_name, $sendFromEmail, $sendFromName) == true){
 	echo "sent";
